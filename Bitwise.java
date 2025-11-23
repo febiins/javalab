beae76dc8803f28636586d1dbe4cba3780ff8fca
febin <febinsunny2004@gmail.com>
2025-11-23 19:59:03 +0530

    refresh Q5 is completede

---
+++ Bitwise.java
@@ -0,0 +1,16 @@
+
+
+public class Bitwise{
+
+	public static void main(String args[]){
+		int a=12;
+		int b=5;
+		
+		System.out.println("a & b="+(a&b)); //AND
+		System.out.println("a | b="+(a|b)); //OR
+		System.out.println("a ^ b="+(a^b)); //XOR
+		System.out.println("a << 1 = " + (a << 1)); //Left shift
+		System.out.println("a >> 1 = " + (a << 1)); //Right shift
+		
+	}
+}
