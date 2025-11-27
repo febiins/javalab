17b6e3fa7549ea7ecd402e32a35c19eea3428822
febin <febinsunny2004@gmail.com>
2025-11-27 22:16:38 +0530

    Q4 is completed

---
+++ Q4.java
@@ -0,0 +1,96 @@
+
+import java.util.*;
+
+public class Q4{
+	int key,temp;
+	int a[],n;
+	
+	Scanner sc = new Scanner(System.in);
+
+	public void read(){
+
+		System.out.println("Enter the number of elements in the array");
+		n=sc.nextInt();
+		a=new int[n];
+
+		System.out.println("Enter the elements of the array");
+
+		for(int i=0;i<n;i++){
+			a[i]=sc.nextInt();
+		}
+		System.out.println("The array is");
+		
+		for(int i=0;i<n;i++){
+			System.out.print(a[i]+"\t");
+		}
+
+
+	}
+	public void search(){
+		System.out.println("Enter the element to search");
+		key=sc.nextInt();
+
+		for(int i=0;i<n;i++){
+			if(a[i]==key){
+				System.out.println("The element is found at position: "+i);	
+			}
+		}
+
+	}
+
+	public void sort(){
+		for(int i=0;i<n-1;i++){
+
+			for(int j=0;j<n-i-1;j++){
+				if(a[j]>a[j+1]){
+					temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+
+		System.out.println("The sorted array is");
+
+		for(int i=0;i<n;i++){
+			System.out.print(a[i]+"\t");
+		}
+		
+
+	}
+
+	public static void main(String args[]){
+		Q4 obj = new Q4();
+        	int ch;
+
+        	do {
+            	System.out.println("\nEnter your choice:");
+            	System.out.println("1. Read elements");
+            	System.out.println("2. Search");
+            	System.out.println("3. Sort");
+            	System.out.println("4. Exit");
+
+            	ch = obj.sc.nextInt();
+
+           	 switch (ch) {
+               	 	case 1:
+                    	 obj.read();
+                    	 break;
+                	case 2:
+                    	 obj.search();
+                    	 break;
+               		case 3:
+                   	 obj.sort();
+                    	 break;
+                	case 4:
+                    	 System.out.println("Exiting...");
+                         break;
+                	default:
+                    	  System.out.println("Invalid choice");
+            		}
+
+       	 	} while (ch != 4);
+		
+	}
+
+}
