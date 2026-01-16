02218d6e53f19a21aa0715e5002f40263c283180
febin <febinsunny2004@gmail.com>
2026-01-16 18:48:20 +0530

    recursion completed

--- RecursionFilePrinting.java
+++ RecursionFilePrinting.java
@@ -1,3 +1,22 @@
+import java.io.*;
+
+
 public class RecursionFilePrinting {
-    
+    public static void printFile(File dir){
+        File [] file = dir.listFiles();
+
+        for(int i=0;i<file.length;i++){
+            File f = file[i];
+            if(f.isDirectory()){
+                System.out.println("Directory: " + f.getAbsolutePath());
+                printFile(f);
+            }else{
+                System.out.println("File: " + f.getAbsolutePath());
+            }
+        }
+    }
+    public static void main(String[] args) {
+        File directory = new File("D:\\testfolder");
+        printFile(directory);
+    }
 }
